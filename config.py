e222ed0788bc99f01261368da25b6944d0a9afef
Shazman Malik <=>
2019-09-28 10:21:32 +0530

    change in config

--- config.py
+++ config.py
@@ -10,4 +10,3 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SERVER_NAME = '127.0.0.1:5000'
