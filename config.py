39528b8f209e252e32aa8d9fe998dddac761f74d
Shazman Malik <=>
2019-09-28 11:09:49 +0530

    change in config

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
 
     SECRET_KEY = "This Is My Key"
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SERVER_NAME = 'imdb-shazman.herokuapp.com'
