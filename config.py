763c118706c92bb5e7d82bbc85d3032b2fbadcdb
Shazman Malik <=>
2019-09-28 09:50:20 +0530

    Commit requirements.txt

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
 
     SECRET_KEY = "This Is My Key"
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SERVER_NAME = '127.0.0.1:5000'
---
+++ readme.txt
@@ -0,0 +1,9 @@
+1) Photo for User in Review
+2) Movie Poster
+3) Actor Photo
+4) Pagination
+5) User Profile
+6) Delete Award
+7) Delete Cast
+8) Filteration
+9) Elastic Search
