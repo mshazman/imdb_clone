e85ccaaef92cc2ee2404ed0bc26bc684bdafe72c
Shazman Malik <=>
2019-09-29 18:19:34 +0530

    This is Working Spike

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config(object):
 
     SECRET_KEY = "This Is My Key"
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SERVER_NAME = 'imdb-shazman.herokuapp.com'
-    # SERVER_NAME = '127.0.0.1:5000'
+    # SERVER_NAME = 'imdb-shazman.herokuapp.com'
+    SERVER_NAME = '127.0.0.1:5000'
