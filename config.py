fa2f2e9c160d1869789e7dff48ff0bfd8517ace5
Shazman Malik <=>
2019-10-06 19:50:16 +0530

    Change Elastic Serach config Variable name

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    ELASTICSEARCH_URL = os.environ.get('ELK_URL')
+    ELASTICSEARCH_URL = os.environ.get('FOUNDELASTICSEARCH_URL')
     CELERY_BROKER_URL = os.environ.get('REDIS_ENV') or 'redis://localhost:6379/'
     CELERY_RESULT_BACKEND = os.environ.get('REDIS_ENV') or 'redis://localhost:6379/'
     CELERY_IMPORTS = ('app.tasks')
